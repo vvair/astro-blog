099adb8506041c0c4504175d7fc69038ed141915
du <vlorcn@gmail.com>
2026-01-18 15:08:38 +0800

    js 组件

---
+++ src/scripts/menu.js
@@ -0,0 +1,8 @@
+const menu = document.querySelector('.menu');
+
+menu?.addEventListener('click', () => {
+  const isExpanded = menu.getAttribute('aria-expanded') === 'true';
+  menu.setAttribute('aria-expanded', `${!isExpanded}`);
+});
+
+console.log(111)
